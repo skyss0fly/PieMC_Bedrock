c09795700d85b68e65e37c9363959e3f29d2db48
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-06-27 09:46:47 +1000

    Create PluginHandler.py

---
+++ src/plugin/PluginHandler.py
@@ -0,0 +1,15 @@
+class PluginHandler():
+  def __init__(self):
+    self.values = PluginHandler.values
+  def __init__(values):
+    values.author = PluginHandler.Author
+    values.api = PluginHandler.Api
+    values.version = PluginHandler.Version
+    values.permissions = PluginHandler.Perms
+    values.Cmd = PluginHandler.Perms
+    values.Prtcl = PluginHandler.Protocol
+  def plugins():
+  self.plugins = plugins
+  def Author():
+    in plugins print(self.plugin.Author)
+  
