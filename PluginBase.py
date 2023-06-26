aafc5508782862cfd1b824f4d556f8158dfb850b
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-06-26 16:27:04 +1000

    Update PluginBase.py

--- PluginBase.py
+++ PluginBase.py
@@ -1,9 +1,12 @@
 class PluginBase():
-  def init():
+  def __init__(self):
+  def plugins():
+    plugins = self.folder.plugins 
+  def init(self):
     print("Loading Plugins:")
     list = {
       plugins.all
     }
-  def Plugins():
+  def Plugins(self):
     print("Plugins:", pluginbase.init.list)
     
