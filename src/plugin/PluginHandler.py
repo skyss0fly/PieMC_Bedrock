363577fe33a8a68e666c5961b4dcaf7359522539
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-06-27 09:53:05 +1000

    Update PluginHandler.py

--- src/plugin/PluginHandler.py
+++ src/plugin/PluginHandler.py
@@ -12,4 +12,11 @@ class PluginHandler():
   self.plugins = plugins
   def Author():
     in plugins print(self.plugin.Author)
-  
+  def Api():
+    in plugins print(self.plugin.Api)
+    if self.plugin.Api != self.version.Api :
+      print("Error, Plugin requires", self.plugin.Api, "Given", self.version.Api)
+      else 
+      print("Loading", self.plugin.Name)
+    load(plugin)
+    
